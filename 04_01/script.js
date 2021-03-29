fbdae3082dd3b98e28404a0cbffd7db5a9457d18
susmita-dutta-cs <dutta.or.susmita@gmail.com>
2021-03-30 01:36:45 +0200

    update

--- 04_01/script.js
+++ 04_01/script.js
@@ -6,7 +6,7 @@
 import Backpack from "./Backpack.js";
 
 const everydayPack = new Backpack(
-  "Everyday Backpack",
+  "greenFrog",
   30,
   "grey",
   15,
@@ -19,3 +19,25 @@ const everydayPack = new Backpack(
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.pocketNum);
 console.log("Days since aquired:", everydayPack.backpackAge());
+// TEMPLATE LITERAL
+// const content = ` <main>
+//       <article>
+//         <h1>${everydayPack.name}</h1>
+//         <ul>
+//           <li>Volume:</li>
+//           <li>Color:</li>
+//           <li>Age:</li>
+//           <li>Number of pockets:</li>
+//           <li>Left strap length:</li>
+//           <li>Right strap length:</li>
+//           <li>Lid status:</li>
+//         </ul>
+//       </article>
+// </main>
+//; mix html and string together and the output will be whatever we want to see on the web page
+
+const content = "<h1>" + everydayPack.name + "</h1>";
+document.body.innerHTML = content;
+// document.body.innerHTML = "Some Text"
+// it removes all the elements appearing in the webpage and shows "some text"
+// but nothing happens to the original file if we reload everything reappears
